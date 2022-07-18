804e41869d36c96eac881fd6ea51b631ccc0a654
Om Madnani <77526602+0mMadnani@users.noreply.github.com>
2022-07-18 22:39:30 +0530

    Add files via upload

---
+++ public/bundle.js
@@ -0,0 +1,74 @@
+const simpleStorageABI = [
+  {
+    "inputs": [],
+    "name": "data",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_data",
+        "type": "string"
+      }
+    ],
+    "name": "set",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "get",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+const simpleStorageAddress = '0x63569cbe2609D35f8822B6217737922537785C5d';
+const web3 = new Web3('http://localhost:9545');
+const simpleStorage = new web3.eth.Contract(simpleStorageABI, simpleStorageAddress);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const $setData = document.getElementById('setData');
+  const $data = document.getElementById('data');
+  let accounts = [];
+
+  web3.eth.getAccounts()
+  .then(_accounts => {
+    accounts = _accounts;
+  });
+
+  const getData = () => {
+    simpleStorage.methods
+      .get()
+      .call()
+      .then(result => {
+        $data.innerHTML = result;
+      })
+  };
+  getData();
+
+  $setData.addEventListener('submit', e => {
+    e.preventDefault();
+    const data = e.target.elements[0].value;
+    simpleStorage.methods
+      .set(data)
+      .send({from: accounts[0]})
+      .then(getData);
+  });
+});
